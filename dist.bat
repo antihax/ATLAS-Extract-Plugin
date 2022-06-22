72ced51691969ecee64fa98164269eaf5e17c361
antihax <antihax@gmail.com>
2022-06-21 21:27:06 -0500

    add dist

--- dist.bat
+++ dist.bat
@@ -3,3 +3,5 @@ copy DumpResources\server\ShooterGame\Binaries\Win64\version.dll DumpResources\D
 copy DumpResources\server\ShooterGame\Binaries\Win64\msdia140.dll DumpResources\DumpResources\configs\
 copy DumpResources\server\ShooterGame\Binaries\Win64\config.json DumpResources\DumpResources\configs\
 copy DumpResources\server\ShooterGame\Binaries\Win64\AtlasApi\Plugins\DumpResources\PdbConfig.json DumpResources\DumpResources\configs\
+
+xcopy DumpResources\server\ShooterGame\Binaries\Win64\AtlasApi\Plugins dist\ /E
