f72b28cc0f4f70196f5dff5f1296706365142c74
antihax <antihax@gmail.com>
2022-06-26 16:00:37 -0500

    update dist

--- dist.bat
+++ dist.bat
@@ -1,7 +1,10 @@
 mkdir dist
-copy DumpResources\server\ShooterGame\Binaries\Win64\version.dll DumpResources\DumpResources\configs\
-copy DumpResources\server\ShooterGame\Binaries\Win64\msdia140.dll DumpResources\DumpResources\configs\
-copy DumpResources\server\ShooterGame\Binaries\Win64\config.json DumpResources\DumpResources\configs\
-copy DumpResources\server\ShooterGame\Binaries\Win64\AtlasApi\Plugins\DumpResources\PdbConfig.json DumpResources\DumpResources\configs\
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\version.dll DumpResources\DumpResources\configs\
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\msdia140.dll DumpResources\DumpResources\configs\
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\config.json DumpResources\DumpResources\configs\
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\AtlasApi\Plugins\DumpResources\PdbConfig.json DumpResources\DumpResources\configs\
 
-xcopy DumpResources\server\ShooterGame\Binaries\Win64\AtlasApi\Plugins dist\ /E
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\AtlasApi\Plugins dist\ /E
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\version.dll dist\
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\msdia140.dll dist\
+xcopy /y DumpResources\server\ShooterGame\Binaries\Win64\config.json dist\
